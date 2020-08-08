d5849a0eac612dc1e4631ae05445d659f307e9b3
Kai Zhang <cskaizhang@gmail.com>
2020-08-08 16:56:20 +0200

    Update basicblock.py

--- models/basicblock.py
+++ models/basicblock.py
@@ -266,6 +266,37 @@ class IMDBlock(nn.Module):
 
 
 # --------------------------------------------
+# Enhanced Spatial Attention (ESA)
+# pay attention to the final inplace operation
+# --------------------------------------------
+class ESA(nn.Module):
+    def __init__(self, channel=64, reduction=4, bias=True):
+        super(ESA, self).__init__()
+        #               -->conv3x3(conv21)-----------------------------------------------------------------------------------------+
+        # conv1x1(conv1)-->conv3x3-2(conv2)-->maxpool7-3-->conv3x3(conv3)(relu)-->conv3x3(conv4)(relu)-->conv3x3(conv5)-->bilinear--->conv1x1(conv6)-->sigmoid
+        self.r_nc = channel // reduction
+        self.conv1 = nn.Conv2d(channel, self.r_nc, kernel_size=1)
+        self.conv21 = nn.Conv2d(self.r_nc, self.r_nc, kernel_size=1)
+        self.conv2 = nn.Conv2d(self.r_nc, self.r_nc, kernel_size=3, stride=2, padding=0)
+        self.conv3 = nn.Conv2d(self.r_nc, self.r_nc, kernel_size=3, padding=1)
+        self.conv4 = nn.Conv2d(self.r_nc, self.r_nc, kernel_size=3, padding=1)
+        self.conv5 = nn.Conv2d(self.r_nc, self.r_nc, kernel_size=3, padding=1)
+        self.conv6 = nn.Conv2d(self.r_nc, channel, kernel_size=1)
+        self.sigmoid = nn.Sigmoid()
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        x1 = self.conv1(x)
+        x2 = F.max_pool2d(self.conv2(x1), kernel_size=7, stride=3)  # 1/6
+        x2 = self.relu(self.conv3(x2))
+        x2 = self.relu(self.conv4(x2))
+        x2 = F.interpolate(self.conv5(x2), (x.size(2), x.size(3)), mode='bilinear', align_corners=False) 
+        x2 = self.conv6(x2 + self.conv21(x1))
+        return x.mul_(self.sigmoid(x2))
+        # return x.mul(self.sigmoid(x2))
+
+
+# --------------------------------------------
 # Channel Attention (CA) Layer
 # --------------------------------------------
 class CALayer(nn.Module):
