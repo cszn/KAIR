3eb3cc7776fa8c57e8ed7c71bfa8039beb4c6677
Kai Zhang <cskaizhang@gmail.com>
2020-09-12 00:10:50 +0200

    Update model_base.py

--- models/model_base.py
+++ models/model_base.py
@@ -101,11 +101,11 @@ class ModelBase():
         if isinstance(network, nn.DataParallel):
             network = network.module
         msg = '\n'
-        msg += ' | {:^6s} | {:^6s} | {:^6s} | {:^6s} || {:<20s}'.format('mean', 'min', 'max', 'std', 'param_name') + '\n'
+        msg += ' | {:^6s} | {:^6s} | {:^6s} | {:^6s} || {:<20s}'.format('mean', 'min', 'max', 'std', 'shape', 'param_name') + '\n'
         for name, param in network.state_dict().items():
             if not 'num_batches_tracked' in name:
                 v = param.data.clone().float()
-                msg += ' | {:>6.3f} | {:>6.3f} | {:>6.3f} | {:>6.3f} || {:s}'.format(v.mean(), v.min(), v.max(), v.std(), name) + '\n'
+                msg += ' | {:>6.3f} | {:>6.3f} | {:>6.3f} | {:>6.3f} | {} || {:s}'.format(v.mean(), v.min(), v.max(), v.std(), v.shape, name) + '\n'
         return msg
 
     """
