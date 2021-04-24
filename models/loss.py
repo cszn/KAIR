c5a64d2dd498855bbd21d165df0354fd5b2b1d0f
Kai Zhang <cskaizhang@gmail.com>
2021-04-25 00:02:23 +0200

    Update PerceptualLoss

--- models/loss.py
+++ models/loss.py
@@ -1,6 +1,60 @@
 import torch
 import torch.nn as nn
+import torchvision
 
+
+# --------------------------------------------
+# Perceptual loss
+# --------------------------------------------
+class VGGFeatureExtractor(nn.Module):
+    def __init__(self, feature_layer=34, use_input_norm=True):
+        super(VGGFeatureExtractor, self).__init__()
+        model = torchvision.models.vgg19(pretrained=True)
+        self.use_input_norm = use_input_norm
+        if self.use_input_norm:
+            mean = torch.Tensor([0.485, 0.456, 0.406]).view(1, 3, 1, 1)
+            # [0.485-1, 0.456-1, 0.406-1] if input in range [-1,1]
+            std = torch.Tensor([0.229, 0.224, 0.225]).view(1, 3, 1, 1)
+            # [0.229*2, 0.224*2, 0.225*2] if input in range [-1,1]
+            self.register_buffer('mean', mean)
+            self.register_buffer('std', std)
+        self.features = nn.Sequential(*list(model.features.children())[:(feature_layer + 1)])
+
+        # No need to BP to variable
+        for k, v in self.features.named_parameters():
+            v.requires_grad = False
+
+    def forward(self, x):
+        if self.use_input_norm:
+            x = (x - self.mean) / self.std
+        output = self.features(x)
+        return output
+
+
+class PerceptualLoss(nn.Module):
+    """Perceptual loss
+    """
+
+    def __init__(self, feature_layer=34, use_input_norm=True, lossfn_type='l1'):
+        super(PerceptualLoss, self).__init__()
+        self.vgg = VGGFeatureExtractor(feature_layer=feature_layer, use_input_norm=use_input_norm)
+        self.lossfn_type = lossfn_type
+        if self.lossfn_type == 'l1':
+            self.lossfn = nn.L1Loss()
+        else:
+            self.lossfn = nn.L2loss()
+
+    def forward(self, x, gt):
+        """Forward function.
+        Args:
+            x (Tensor): Input tensor with shape (n, c, h, w).
+            gt (Tensor): Ground-truth tensor with shape (n, c, h, w).
+        Returns:
+            Tensor: Forward results.
+        """
+        x_vgg, gt_vgg = self.vgg(x), self.vgg(gt.detach())
+        loss = self.lossfn(x_vgg, gt_vgg)
+        return loss
 
 # --------------------------------------------
 # GAN loss: gan, ragan
