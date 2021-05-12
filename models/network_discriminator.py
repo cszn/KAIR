48c72015673f604c85732d5477fb003698c79ea6
Kai Zhang <cskaizhang@gmail.com>
2021-05-12 19:50:12 +0200

    Update network_discriminator.py

--- models/network_discriminator.py
+++ models/network_discriminator.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 from torch.nn.utils import spectral_norm
 import models.basicblock as B
+import functools
+import numpy as np
 
 
 """
@@ -12,6 +14,88 @@ import models.basicblock as B
 # Discriminator_VGG_128_SN
 # --------------------------------------------
 """
+
+
+# --------------------------------------------
+# PatchGAN discriminator
+# If n_layers = 3, then the receptive field is 70x70
+# --------------------------------------------
+class Discriminator_PatchGAN(nn.Module):
+    def __init__(self, input_nc=3, ndf=64, n_layers=3, norm_type='batch'):
+        super(Discriminator_PatchGAN, self).__init__()
+        self.n_layers = n_layers
+
+        '''
+        'batch'
+        'instance'
+        'spectral'
+        'batchspectral'
+        'none'
+        '''
+        if norm_type == 'batch':
+            norm_layer = functools.partial(nn.BatchNorm2d, affine=True, track_running_stats=True)
+            use_sp_norm = False
+            use_bias = False
+        elif norm_type == 'instance':
+            norm_layer = functools.partial(nn.InstanceNorm2d, affine=False, track_running_stats=False)
+            use_sp_norm = False
+            use_bias = True
+        elif norm_type == 'spectral':
+            norm_layer = None
+            use_sp_norm = True
+            use_bias = True
+        elif norm_type == 'batchspectral':
+            norm_layer = functools.partial(nn.BatchNorm2d, affine=True, track_running_stats=True)
+            use_sp_norm = True
+            use_bias = False
+        elif norm_type == 'none':
+            norm_layer = None
+            use_sp_norm = False
+            use_bias = True
+        else:
+            raise NotImplementedError('normalization layer [%s] is not found' % norm_type)
+
+        kw = 4
+        padw = int(np.ceil((kw-1.0)/2))
+        sequence = [
+            self.use_spectral_norm(nn.Conv2d(input_nc, ndf, kernel_size=kw, stride=2, padding=padw), use_sp_norm),
+            nn.LeakyReLU(0.2)
+        ]
+
+        nf_mult = 1
+        nf_mult_prev = 1
+        for n in range(1, n_layers):
+            nf_mult_prev = nf_mult
+            nf_mult = min(2**n, 8)
+            sequence += [
+                self.use_spectral_norm(nn.Conv2d(ndf * nf_mult_prev, ndf * nf_mult, kernel_size=kw, stride=2, padding=padw, bias=use_bias), use_sp_norm),
+                norm_layer(ndf * nf_mult) if norm_layer else None,
+                nn.LeakyReLU(0.2)
+            ]
+
+        nf_mult_prev = nf_mult
+        nf_mult = min(2**n_layers, 8)
+        sequence += [
+            self.use_spectral_norm(nn.Conv2d(ndf * nf_mult_prev, ndf * nf_mult, kernel_size=kw, stride=1, padding=padw, bias=use_bias), use_sp_norm),
+            norm_layer(ndf * nf_mult) if norm_layer else None,
+            nn.LeakyReLU(0.2)
+        ]
+        sequence += [self.use_spectral_norm(nn.Conv2d(ndf * nf_mult, 1, kernel_size=kw, stride=1, padding=padw), use_sp_norm)]
+        
+        sequence_new = []
+        for n in range(len(sequence)):
+            if sequence[n] is not None:
+                sequence_new.append(sequence[n])
+
+        self.model = nn.Sequential(*sequence_new)
+
+    def use_spectral_norm(self, module, mode=False):
+        if mode:
+            return spectral_norm(module)
+        return module
+
+    def forward(self, input):
+        return self.model(input)
 
 
 # --------------------------------------------
