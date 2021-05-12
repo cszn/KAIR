9c81d0fb3efe7df25edcb9faab34fcf4f24a1154
Kai Zhang <cskaizhang@gmail.com>
2021-05-12 02:20:17 +0200

    Update model_gan.py

--- models/model_gan.py
+++ models/model_gan.py
@@ -205,11 +205,12 @@ class ModelGAN(ModelBase):
                 F_loss = self.F_lossfn_weight * self.F_lossfn(self.E, self.H)
                 loss_G_total += F_loss                 # 2) VGG feature loss
 
-            pred_g_fake = self.netD(self.E)
             if self.opt['train']['gan_type'] in ['gan', 'lsgan', 'wgangp']:
+                pred_g_fake = self.netD(self.E)
                 D_loss = self.D_lossfn_weight * self.D_lossfn(pred_g_fake, True)
             elif self.opt['train']['gan_type'] == 'ragan':
-                pred_d_real = self.netD(self.var_ref).detach()
+                pred_d_real = self.netD(self.H).detach()
+                pred_g_fake = self.netD(self.E)
                 D_loss = self.D_lossfn_weight * (
                     self.D_lossfn(pred_d_real - torch.mean(pred_g_fake), False) +
                     self.D_lossfn(pred_g_fake - torch.mean(pred_d_real), True)) / 2
@@ -235,7 +236,7 @@ class ModelGAN(ModelBase):
             # tensor for calculating mean.
             if self.opt_train['gan_type'] in ['gan', 'lsgan', 'wgan', 'softplusgan']:
                 # real
-                pred_d_real = self.netD(self.var_ref)          # 1) real data
+                pred_d_real = self.netD(self.H)          # 1) real data
                 l_d_real = self.D_lossfn(pred_d_real, True)
                 l_d_real.backward()
                 # fake
@@ -244,8 +245,8 @@ class ModelGAN(ModelBase):
                 l_d_fake.backward()
             elif self.opt_train['gan_type'] == 'ragan':
                 # real
-                pred_d_real = self.netD(self.var_ref)          # 1) real data
-                pred_d_fake = self.netD(self.E).detach()       # 2) fake data, detach to avoid BP to G
+                pred_d_fake = self.netD(self.E).detach()       # 1) fake data, detach to avoid BP to G
+                pred_d_real = self.netD(self.H)          # 2) real data
                 l_d_real = 0.5 * self.D_lossfn(pred_d_real - torch.mean(pred_d_fake), True)
                 l_d_real.backward()
                 # fake
