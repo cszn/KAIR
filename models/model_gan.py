fa74e0c0a0085ae5d07314be85dd79989fd9f8e8
Kai Zhang <cskaizhang@gmail.com>
2021-08-23 23:46:40 +0200

    minor

--- models/model_gan.py
+++ models/model_gan.py
@@ -206,15 +206,13 @@ class ModelGAN(ModelBase):
 
             if self.opt['train']['gan_type'] in ['gan', 'lsgan', 'wgan', 'softplusgan']:
                 pred_g_fake = self.netD(self.E)
-                D_loss = 0.0
                 D_loss = self.D_lossfn_weight * self.D_lossfn(pred_g_fake, True)
             elif self.opt['train']['gan_type'] == 'ragan':
-                pred_d_real = self.netD(self.H)
+                pred_d_real = self.netD(self.H).detach()
                 pred_g_fake = self.netD(self.E)
-                D_loss = 0.0
                 D_loss = self.D_lossfn_weight * (
-                        self.D_lossfn(pred_d_real.detach() - torch.mean(pred_g_fake, 0, True), False) +
-                        self.D_lossfn(pred_g_fake - torch.mean(pred_d_real.detach(), 0, True), True)) / 2
+                        self.D_lossfn(pred_d_real - torch.mean(pred_g_fake, 0, True), False) +
+                        self.D_lossfn(pred_g_fake - torch.mean(pred_d_real, 0, True), True)) / 2
             loss_G_total += D_loss                    # 3) GAN loss
 
             loss_G_total.backward()
@@ -228,45 +226,31 @@ class ModelGAN(ModelBase):
 
         self.D_optimizer.zero_grad()
 
-        if self.opt['dist']:
-            # In order to avoid the error in distributed training:
-            # "Error detected in CudnnBatchNormBackward: RuntimeError: one of
-            # the variables needed for gradient computation has been modified by
-            # an inplace operation",
-            # we separate the backwards for real and fake, and also detach the
-            # tensor for calculating mean.
-            if self.opt_train['gan_type'] in ['gan', 'lsgan', 'wgan', 'softplusgan']:
-                # real
-                pred_d_real = self.netD(self.H)                # 1) real data
-                l_d_real = self.D_lossfn(pred_d_real, True)
-                l_d_real.backward()
-                # fake
-                pred_d_fake = self.netD(self.E.detach())       # 2) fake data, detach to avoid BP to G
-                l_d_fake = self.D_lossfn(pred_d_fake, False)
-                l_d_fake.backward()
-            elif self.opt_train['gan_type'] == 'ragan':
-                # real
-                pred_d_fake = self.netD(self.E)       # 1) fake data, detach to avoid BP to G
-                pred_d_real = self.netD(self.H)       # 2) real data
-                l_d_real = 0.5 * self.D_lossfn(pred_d_real - torch.mean(pred_d_fake.detach(), 0, True), True)
-                l_d_real.backward()
-                # fake
-                pred_d_fake = self.netD(self.E.detach())
-                l_d_fake = 0.5 * self.D_lossfn(pred_d_fake - torch.mean(pred_d_real.detach(), 0, True), False)
-                l_d_fake.backward()
-        else:
-            loss_D_total = 0
-            pred_d_fake = self.netD(self.E.detach())           # 1) fake data, detach to avoid BP to G
-            pred_d_real = self.netD(self.H)                    # 2) real data
-            if self.opt_train['gan_type'] in ['gan', 'lsgan', 'wgan', 'softplusgan']:
-                l_d_real = self.D_lossfn(pred_d_real, True)
-                l_d_fake = self.D_lossfn(pred_d_fake, False)
-                loss_D_total = l_d_real + l_d_fake
-            elif self.opt_train['gan_type'] == 'ragan':
-                l_d_real = self.D_lossfn(pred_d_real - torch.mean(pred_d_fake, 0, True), True)
-                l_d_fake = self.D_lossfn(pred_d_fake - torch.mean(pred_d_real, 0, True), False)
-                loss_D_total = (l_d_real + l_d_fake) / 2
-            loss_D_total.backward()
+        # In order to avoid the error in distributed training:
+        # "Error detected in CudnnBatchNormBackward: RuntimeError: one of
+        # the variables needed for gradient computation has been modified by
+        # an inplace operation",
+        # we separate the backwards for real and fake, and also detach the
+        # tensor for calculating mean.
+        if self.opt_train['gan_type'] in ['gan', 'lsgan', 'wgan', 'softplusgan']:
+            # real
+            pred_d_real = self.netD(self.H)                # 1) real data
+            l_d_real = self.D_lossfn(pred_d_real, True)
+            l_d_real.backward()
+            # fake
+            pred_d_fake = self.netD(self.E.detach().clone()) # 2) fake data, detach to avoid BP to G
+            l_d_fake = self.D_lossfn(pred_d_fake, False)
+            l_d_fake.backward()
+        elif self.opt_train['gan_type'] == 'ragan':
+            # real
+            pred_d_fake = self.netD(self.E).detach()       # 1) fake data, detach to avoid BP to G
+            pred_d_real = self.netD(self.H)                # 2) real data
+            l_d_real = 0.5 * self.D_lossfn(pred_d_real - torch.mean(pred_d_fake, 0, True), True)
+            l_d_real.backward()
+            # fake
+            pred_d_fake = self.netD(self.E.detach())
+            l_d_fake = 0.5 * self.D_lossfn(pred_d_fake - torch.mean(pred_d_real.detach(), 0, True), False)
+            l_d_fake.backward()
 
         self.D_optimizer.step()
 
