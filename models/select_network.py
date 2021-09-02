88677b25d67d929862bbfbd097d065b808792d55
Kai Zhang <cskaizhang@gmail.com>
2021-09-02 21:32:38 +0200

    Add SwinIR

--- models/select_network.py
+++ models/select_network.py
@@ -184,6 +184,22 @@ def define_G(opt):
                    upsample_mode=opt_net['upsample_mode'],
                    bias=opt_net['bias'])
 
+    # ----------------------------------------
+    # SwinIR
+    # ----------------------------------------
+    elif net_type == 'swinir':
+        from models.network_swinir import SwinIR as net
+        netG = net(upscale=opt_net['upscale'],
+                   in_chans=opt_net['in_chans'],
+                   img_size=opt_net['img_size'],
+                   window_size=opt_net['window_size'],
+                   img_range=opt_net['img_range'],
+                   depths=opt_net['depths'],
+                   embed_dim=opt_net['embed_dim'],
+                   num_heads=opt_net['num_heads'],
+                   mlp_ratio=opt_net['mlp_ratio'],
+                   upsampler=opt_net['upsampler'],
+                   resi_connection=opt_net['resi_connection'])
 
     # ----------------------------------------
     # others
