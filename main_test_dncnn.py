3717b5881cb3fa3c6e05ac715731c7263c99cf0a
Kai Zhang <cskaizhang@gmail.com>
2021-09-07 22:26:26 +0200

    Support argparse

--- main_test_dncnn.py
+++ main_test_dncnn.py
@@ -1,5 +1,6 @@
 import os.path
 import logging
+import argparse
 
 import numpy as np
 from datetime import datetime
@@ -65,45 +66,44 @@ def main():
     # ----------------------------------------
     # Preparation
     # ----------------------------------------
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_name', type=str, default='dncnn_25', help='dncnn_15, dncnn_25, dncnn_50, dncnn_gray_blind, dncnn_color_blind, dncnn3')
+    parser.add_argument('--testset_name', type=str, default='set12', help='test set, bsd68 | set12')
+    parser.add_argument('--noise_level_img', type=int, default=15, help='noise level: 15, 25, 50')
+    parser.add_argument('--x8', type=bool, default=False, help='x8 to boost performance')
+    parser.add_argument('--show_img', type=bool, default=False, help='show the image')
+    parser.add_argument('--model_pool', type=str, default='model_zoo', help='path of model_zoo')
+    parser.add_argument('--testsets', type=str, default='testsets', help='path of testing folder')
+    parser.add_argument('--results', type=str, default='results', help='path of results')
+    parser.add_argument('--need_degradation', type=bool, default=True, help='add noise or not')
+    parser.add_argument('--task_current', type=str, default='dn', help='dn for denoising, fixed!')
+    parser.add_argument('--sf', type=int, default=1, help='unused for denoising')
+    args = parser.parse_args()
 
-    noise_level_img = 25             # noise level for noisy image
-    model_name = 'dncnn_25'          # 'dncnn_15' | 'dncnn_25' | 'dncnn_50' | 'dncnn_gray_blind' | 'dncnn_color_blind' | 'dncnn3'
-    testset_name = 'bsd68'           # test set, 'bsd68' | 'set12'
-    need_degradation = True          # default: True
-    x8 = False                       # default: False, x8 to boost performance
-    show_img = False                 # default: False
-
-
-
-
-    task_current = 'dn'       # 'dn' for denoising | 'sr' for super-resolution
-    sf = 1                    # unused for denoising
-    if 'color' in model_name:
-        n_channels = 3        # fixed, 1 for grayscale image, 3 for color image 
+    if 'color' in args.model_name:
+        n_channels = 3        # fixed, 1 for grayscale image, 3 for color image
     else:
-        n_channels = 1        # fixed for grayscale image 
-    if model_name in ['dncnn_gray_blind', 'dncnn_color_blind', 'dncnn3']:
+        n_channels = 1        # fixed for grayscale image
+    if args.model_name in ['dncnn_gray_blind', 'dncnn_color_blind', 'dncnn3']:
         nb = 20               # fixed
     else:
         nb = 17               # fixed
-    model_pool = 'model_zoo'  # fixed
-    testsets = 'testsets'     # fixed
-    results = 'results'       # fixed
-    result_name = testset_name + '_' + model_name     # fixed
-    border = sf if task_current == 'sr' else 0        # shave boader to calculate PSNR and SSIM
-    model_path = os.path.join(model_pool, model_name+'.pth')
+
+    result_name = args.testset_name + '_' + args.model_name     # fixed
+    border = args.sf if args.task_current == 'sr' else 0        # shave boader to calculate PSNR and SSIM
+    model_path = os.path.join(args.model_pool, args.model_name+'.pth')
 
     # ----------------------------------------
     # L_path, E_path, H_path
     # ----------------------------------------
 
-    L_path = os.path.join(testsets, testset_name) # L_path, for Low-quality images
+    L_path = os.path.join(args.testsets, args.testset_name) # L_path, for Low-quality images
     H_path = L_path                               # H_path, for High-quality images
-    E_path = os.path.join(results, result_name)   # E_path, for Estimated images
+    E_path = os.path.join(args.results, result_name)   # E_path, for Estimated images
     util.mkdir(E_path)
 
     if H_path == L_path:
-        need_degradation = True
+        args.need_degradation = True
     logger_name = result_name
     utils_logger.logger_info(logger_name, log_path=os.path.join(E_path, logger_name+'.log'))
     logger = logging.getLogger(logger_name)
@@ -131,7 +131,7 @@ def main():
     test_results['psnr'] = []
     test_results['ssim'] = []
 
-    logger.info('model_name:{}, image sigma:{}'.format(model_name, noise_level_img))
+    logger.info('model_name:{}, image sigma:{}'.format(args.model_name, args.noise_level_img))
     logger.info(L_path)
     L_paths = util.get_image_paths(L_path)
     H_paths = util.get_image_paths(H_path) if need_H else None
@@ -147,11 +147,11 @@ def main():
         img_L = util.imread_uint(img, n_channels=n_channels)
         img_L = util.uint2single(img_L)
 
-        if need_degradation:  # degradation process
+        if args.need_degradation:  # degradation process
             np.random.seed(seed=0)  # for reproducibility
-            img_L += np.random.normal(0, noise_level_img/255., img_L.shape)
+            img_L += np.random.normal(0, args.noise_level_img/255., img_L.shape)
 
-        util.imshow(util.single2uint(img_L), title='Noisy image with noise level {}'.format(noise_level_img)) if show_img else None
+        util.imshow(util.single2uint(img_L), title='Noisy image with noise level {}'.format(args.noise_level_img)) if args.show_img else None
 
         img_L = util.single2tensor4(img_L)
         img_L = img_L.to(device)
@@ -160,7 +160,7 @@ def main():
         # (2) img_E
         # ------------------------------------
 
-        if not x8:
+        if not args.x8:
             img_E = model(img_L)
         else:
             img_E = utils_model.test_mode(model, img_L, mode=3)
@@ -185,7 +185,7 @@ def main():
             test_results['psnr'].append(psnr)
             test_results['ssim'].append(ssim)
             logger.info('{:s} - PSNR: {:.2f} dB; SSIM: {:.4f}.'.format(img_name+ext, psnr, ssim))
-            util.imshow(np.concatenate([img_E, img_H], axis=1), title='Recovered / Ground-truth') if show_img else None
+            util.imshow(np.concatenate([img_E, img_H], axis=1), title='Recovered / Ground-truth') if args.show_img else None
 
         # ------------------------------------
         # save results
