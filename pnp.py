613f0aac2d579da25585b6eccef3983e0bab2413
Shiyang <winsanyu@hotmail.com>
2022-12-26 02:20:11 +0800

    init search_args

--- pnp.py
+++ pnp.py
@@ -8,6 +8,8 @@ from torch.utils.data import DataLoader
 from tqdm import tqdm
 import logging
 import os.path
+from collections import OrderedDict
+from copy import deepcopy
 
 from utils import utils_option as option
 from utils import utils_image as util
@@ -20,7 +22,7 @@ def get_opt(json_path='options/pnp/pnp_drunet.json'):
     opt = option.dict_to_nonedict(opt)
     return opt
 
-def get_model(opt):
+def get_network(opt):
     model = define_G(opt)
     model_path = opt['path']['pretrained_netG']
     model.load_state_dict(torch.load(model_path), strict=True)
@@ -102,88 +104,98 @@ class PNP_ADMM(nn.Module):
         ssim = util.calculate_ssim(img_E, img_H, border=0)
         return psnr, ssim
 
-def evaluate():
-    # H_paths = opt['datasets']['test']['dataroot_H']
-    # H_paths = util.get_image_paths(H_paths)
-    # L_paths = H_paths
-    # noise_level_img = opt['sigma_test']
-    # n_channels = opt['n_channels']
-
-    # for idx, img in enumerate(L_paths):
-
-    #     # ------------------------------------
-    #     # (1) img_L
-    #     # ------------------------------------
-
-    #     img_name, ext = os.path.splitext(os.path.basename(img))
-    #     # logger.info('{:->4d}--> {:>10s}'.format(idx+1, img_name+ext))
-    #     img_L = util.imread_uint(img, n_channels=1)
-    #     img_L = util.uint2single(img_L)
-
-    #     np.random.seed(seed=0)  # for reproducibility
-    #     img_L += np.random.normal(0, noise_level_img/255., img_L.shape)
-
-    #     # util.imshow(util.single2uint(img_L), title='Noisy image with noise level {}'.format(noise_level_img)) if show_img else None
-
-    #     img_L = util.single2tensor4(img_L)
-    #     noise_level_map = torch.ones((1, 1, img_L.size(2), img_L.size(3)), dtype=torch.float).mul_(noise_level_img/255.)
-    #     img_L = torch.cat((img_L, noise_level_map), dim=1)
-    #     img_L = img_L.to(device)
-
-    #     # ------------------------------------
-    #     # (2) img_E
-    #     # ------------------------------------
-
-
-    #     img_E = model(img_L)
-
-    #     img_E = util.tensor2uint(img_E)
-
-    #     if 1:
-
-    #         # --------------------------------
-    #         # (3) img_H
-    #         # --------------------------------
-
-    #         img_H = util.imread_uint(H_paths[idx], n_channels=n_channels)
-    #         img_H = img_H.squeeze()
-
-    #         # --------------------------------
-    #         # PSNR and SSIM
-    #         # --------------------------------
-
-    #         psnr = util.calculate_psnr(img_E, img_H, border=0)
-    #         ssim = util.calculate_ssim(img_E, img_H, border=0)
-    #         logger.info('{:s} - PSNR: {:.2f} dB; SSIM: {:.4f}.'.format(img_name+ext, psnr, ssim))
-    pass
-
-def main():
+def evaluate(opt):
     device = 'cuda'
-    opt = get_opt()
     test_loader = get_test_loader(opt)
-    model = get_model(opt)
-    model.to(device)
+    network = get_network(opt)
+    network.to(device)
     logger = gen_logger(opt)
+    pnp_admm = PNP_ADMM(network, opt['pnp'])
     # logger.info(option.dict2str(opt))
-    # logger.info(model)
-    pnp_admm = PNP_ADMM(model, opt['pnp'])
+    # logger.info(network)
 
-    for test_data in test_loader:
-        image_name_ext = os.path.basename(test_data['L_path'][0])
-        img_name, ext = os.path.splitext(image_name_ext)
+    H_paths = opt['datasets']['test']['dataroot_H']
+    H_paths = util.get_image_paths(H_paths)
+    L_paths = H_paths
+    noise_level_img = opt['sigma_test']
+    n_channels = opt['n_channels']
 
-        img_L = test_data['L'].to(device)
-        img_H = test_data['H']
+    test_results = OrderedDict()
+    test_results['psnr'] = []
+    test_results['ssim'] = []
+
+    for idx, img in enumerate(L_paths):
+
+        # ------------------------------------
+        # (1) img_L
+        # ------------------------------------
+
+        img_name, ext = os.path.splitext(os.path.basename(img))
+        # logger.info('{:->4d}--> {:>10s}'.format(idx+1, img_name+ext))
+        img_L = util.imread_uint(img, n_channels=1)
+        img_L = util.uint2single(img_L)
+
+        np.random.seed(seed=0)  # for reproducibility
+        img_L += np.random.normal(0, noise_level_img/255., img_L.shape)
+
+        # util.imshow(util.single2uint(img_L), title='Noisy image with noise level {}'.format(noise_level_img)) if show_img else None
+
+        img_L = util.single2tensor4(img_L)
+        noise_level_map = torch.ones((1, 1, img_L.size(2), img_L.size(3)), dtype=torch.float).mul_(noise_level_img/255.)
+        img_L = torch.cat((img_L, noise_level_map), dim=1)
+        img_L = img_L.to(device)
+
+        # ------------------------------------
+        # (2) img_E
+        # ------------------------------------
+
 
         img_E = pnp_admm(img_L)
 
         img_E = util.tensor2uint(img_E)
-        img_H = util.tensor2uint(img_H)
+
+
+        # --------------------------------
+        # (3) img_H
+        # --------------------------------
+
+        img_H = util.imread_uint(H_paths[idx], n_channels=n_channels)
+        img_H = img_H.squeeze()
+
+        # --------------------------------
+        # PSNR and SSIM
+        # --------------------------------
 
         psnr = util.calculate_psnr(img_E, img_H, border=0)
         ssim = util.calculate_ssim(img_E, img_H, border=0)
+        test_results['psnr'].append(psnr)
+        test_results['ssim'].append(ssim)
         logger.info('{:s} - PSNR: {:.2f} dB; SSIM: {:.4f}.'.format(img_name+ext, psnr, ssim))
-        
 
+    ave_psnr = sum(test_results['psnr']) / len(test_results['psnr'])
+    ave_ssim = sum(test_results['ssim']) / len(test_results['ssim'])
+    logger.info('Average PSNR/SSIM(RGB) - PSNR: {:.2f} dB; SSIM: {:.4f}'.format(ave_psnr, ave_ssim))
+    
+    return ave_psnr, ave_ssim
+
+def search_args():
+    # TODO: get_opts
+    opt = get_opt()
+    opts = [opt,]
+    opt_max_psnr = None
+    opt_max_ssim = None
+    max_psnr = 0.
+    max_ssim = 0.
+    for opt in opts:
+        cur_psnr, cur_ssim = evaluate(opt)
+        if cur_psnr > max_psnr:
+            max_psnr = cur_psnr
+            opt_max_psnr = deepcopy(opt)
+        if cur_ssim > max_ssim:
+            max_ssim = cur_ssim
+            opt_max_ssim = deepcopy(opt)
+
+    print(max_psnr, max_ssim)
+    
 if __name__ == '__main__':
-    main()
+    search_args()
